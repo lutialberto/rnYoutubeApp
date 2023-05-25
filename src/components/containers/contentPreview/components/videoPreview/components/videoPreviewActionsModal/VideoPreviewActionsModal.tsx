8b593e49ade77df452d8796808927fc4704d7c15
Alberto Luti <alberto_luti@edsa.com.ar>
2023-05-25 13:34:44 -0300

    config video preview action items

--- src/components/containers/contentPreview/components/videoPreview/components/videoPreviewActionsModal/VideoPreviewActionsModal.tsx
+++ src/components/containers/contentPreview/components/videoPreview/components/videoPreviewActionsModal/VideoPreviewActionsModal.tsx
@@ -1,6 +1,15 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import ModalWrapper from '~/components/containers/modals/modalWrapper/ModalWrapper';
+import ClockIcon from '~/assets/icons/Clock.svg';
+import DownloadIcon from '~/assets/icons/Download.svg';
+import ShareIcon from '~/assets/icons/Share.svg';
+import FlagIcon from '~/assets/icons/Flag.svg';
+import BanIcon from '~/assets/icons/Ban.svg';
+import ListIcon from '~/assets/icons/List.svg';
+import SquarePlusIcon from '~/assets/icons/SquarePlus.svg';
+import UserSlashIcon from '~/assets/icons/UserSlash.svg';
+import {colors} from '~/constants/Colors';
 
 interface IVideoPreviewActionsModalProps {
   show: boolean;
@@ -8,13 +17,80 @@ interface IVideoPreviewActionsModalProps {
 }
 
 const VideoPreviewActionsModal = (props: IVideoPreviewActionsModalProps) => {
+  const actions = [
+    {
+      icon: <ListIcon style={styles.icon} />,
+      title: 'Reproducir el próximo video de la fila',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <ClockIcon style={styles.icon} />,
+      title: 'Guardar en Ver más tarde',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <SquarePlusIcon style={styles.icon} />,
+      title: 'Guardar en una lista de reproducción',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <DownloadIcon style={styles.icon} />,
+      title: 'Descargar video',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <ShareIcon style={styles.icon} />,
+      title: 'Compartir',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <BanIcon style={styles.icon} />,
+      title: 'No me interesa',
+      onPress: () => console.log('dsa'),
+    },
+    {
+      icon: <UserSlashIcon style={styles.icon} />,
+      title: 'No recomendar el canal',
+      onPress: () => console.log('dsa'),
+    },
+    {icon: <FlagIcon style={styles.icon} />, title: 'Denunciar', onPress: () => console.log('dsa')},
+  ];
   return (
-    <ModalWrapper show={props.show} onClose={props.onClose}>
-      <Text>VideoPreviewActionsModal</Text>
+    <ModalWrapper
+      show={props.show}
+      onClose={props.onClose}
+      childrenContainerStyles={styles.container}>
+      {actions.map(action => (
+        <Pressable key={action.title} style={styles.actionContainer} onPress={action.onPress}>
+          {action.icon}
+          <Text style={styles.title}>{action.title}</Text>
+        </Pressable>
+      ))}
     </ModalWrapper>
   );
 };
 
 export default VideoPreviewActionsModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    height: 'auto',
+    gap: 20,
+    backgroundColor: colors.cardBackground,
+    margin: 5,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  icon: {
+    color: colors.font,
+    aspectRatio: 1,
+    width: 20,
+  },
+  title: {
+    color: colors.font,
+  },
+});
--- src/components/containers/modals/modalWrapper/ModalWrapper.tsx
+++ src/components/containers/modals/modalWrapper/ModalWrapper.tsx
@@ -1,4 +1,4 @@
-import {Modal, Pressable, StyleSheet, View} from 'react-native';
+import {Modal, Pressable, StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
 import React from 'react';
 import {colors} from '~/constants/Colors';
 import {vh} from '~/constants/Dimensions';
@@ -7,13 +7,14 @@ interface IModalWrapperProps {
   show: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  childrenContainerStyles?: StyleProp<ViewStyle>;
 }
 
 const ModalWrapper = (props: IModalWrapperProps) => {
   return (
     <Modal animationType="slide" transparent={true} visible={props.show}>
       <Pressable onPress={props.onClose} style={styles.pressableClose} />
-      <View style={styles.modal}>{props.children}</View>
+      <View style={[styles.modal, props.childrenContainerStyles]}>{props.children}</View>
     </Modal>
   );
 };
@@ -22,12 +23,12 @@ export default ModalWrapper;
 
 const styles = StyleSheet.create({
   pressableClose: {
-    height: vh * 25,
+    flex: 1,
     width: '100%',
   },
   modal: {
+    height: vh * 75,
     backgroundColor: colors.background,
-    flex: 1,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
   },
